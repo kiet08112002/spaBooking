6c57ecf9e9e56d99b157621b51340cb398c972ab
nhvu2001 <vunguyendienlam@gmail.com>
2024-04-14 20:40:36 +0700

    data

--- configs/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    SECRET_KEY: 'KIET123',
-    EXPIRE_JWT: '1d',
-    hostName: 'localhost:3000',
-    Host: "sandbox.smtp.mailtrap.io",
-    Port: 25,
-    Username: "af96fd34aba17d",
-    Password: "181df2ce8a7473"
-}
